38c40076072f676c87b3d6b4a37cb0531b43ec28
Jose Salvatierra <jsf.pbt>
2012-10-28 08:13:41 +0000

    Added the next 2 projects I'll work on
    JSFHarpoon - Buys harpoons from Gerrant and deposits in Port Sarim
    JSFGoblins - Tutorial script that kills goblins in Lumbridge

---
+++ JSFGoblins/JSFGoblins.java
@@ -0,0 +1,13 @@
+package JSFGoblins;
+
+import org.powerbot.core.script.ActiveScript;
+
+public class JSFGoblins extends ActiveScript {
+
+	@Override
+	public int loop() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ JSFHarpoon/JSFHarpoon.java
@@ -0,0 +1,13 @@
+package JSFHarpoon;
+
+import org.powerbot.core.script.ActiveScript;
+
+public class JSFHarpoon extends ActiveScript {
+
+	@Override
+	public int loop() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
